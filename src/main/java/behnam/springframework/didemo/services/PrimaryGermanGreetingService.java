5bd7feee02f9cb018a1e3609a7dd1ed92b5045da
behnam <behnamsh72@gmail.com>
2020-02-29 01:41:05 +0330

    dafault profile has been set

--- src/main/java/behnam/springframework/didemo/services/PrimaryGermanGreetingService.java
+++ src/main/java/behnam/springframework/didemo/services/PrimaryGermanGreetingService.java
@@ -5,14 +5,13 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-
-//using primary annotaion if there is more than one bean we used primary key or Qualifier
 @Service
 @Primary
-@Profile({"en","default"})
-public class PrimaryGreetingService implements GreetingService {
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService {
+
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting service";
+        return "Primarer grudines";
     }
 }
